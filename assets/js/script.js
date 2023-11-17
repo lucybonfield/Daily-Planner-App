a426f116f7d5824f7dde391dc2323ce58c8c71f1
bonfield2l <lucy.bonfield2@paddypowerbetfair.com>
2023-11-17 17:32:59 +0000

    added rows for tasks, with user input and empty button. Added times to rows, incrementing by 1 hour each row to go up to 5pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
+// Display current day at the top of page
 var currentDay = dayjs().format("dddd, MMMM D");
 $("#currentDay").text(currentDay);
+
+// Task rows
+const startTime = 9;
+const endTime = 17;
+for (let hour = startTime; hour <= endTime; hour++) {
+  const originalTime = dayjs().hour(hour).minute(0).second(0);
+  const formattedTime = originalTime.format("hA");
+  
+  const newRow = `<tr>
+                    <td class="time${hour - startTime + 1}">${formattedTime}</td>
+                    <td class="task${hour - startTime + 1}"><input type="text" id="task${hour}" /></td>
+                    <td class="save${hour - startTime + 1}"><button onclick="saveTask(${hour})">Save</button></td>
+                 </tr>`;
+  $(".container table").append(newRow);
+}
+
+// Save
+function saveTask(hour) {
+  const taskInput = $(`#task${hour}`).val();
+  // Do something with the task input
+}
