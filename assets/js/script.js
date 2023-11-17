661da35b4b43a30b95195241025dec655638e997
bonfield2l <lucy.bonfield2@paddypowerbetfair.com>
2023-11-17 21:03:01 +0000

    fixed buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const startTime = 9;
 const endTime = 17;
 
+// Display current day at the top of page
+var currentDay = dayjs().format("dddd, MMMM D");
+$("#currentDay").text(currentDay);
+$("#currentDay").text(currentDay);
+
 // Load tasks from local storage
 function loadTasks() {
   for (let hour = startTime; hour <= endTime; hour++) {
@@ -27,23 +32,26 @@ for (let hour = startTime; hour <= endTime; hour++) {
   const currentHour = dayjs().hour();
   const originalTime = dayjs().hour(hour).minute(0).second(0);
   const formattedTime = originalTime.format("hA");
-  // Determine the background color based on the current time
-  let bgColorClass;
+
+  // Change background colour depending on current time
+  let bgColour;
   if (currentHour > hour) {
-    bgColorClass = 'past'; // Time has passed
+    bgColour = 'past'; // Time has passed
   } else if (currentHour === hour) {
-    bgColorClass = 'present'; // Current hour
+    bgColour = 'present'; // Current hour
   } else {
-    bgColorClass = 'future'; // Future time
+    bgColour = 'future'; // Future time
   }
 
   // Create new rows with incrementing times
-  const newRow = `<tr>
-                    <td class="time-block hour">${formattedTime}</td>
-                    <td class="time-block task"><input type="text" id="task${hour}" /></td>
-                    <td class="time-block saveBtn"><button onclick="saveTask(${hour})">Save</button></td>
+  const newRow = `<tr class="time-block">
+                    <td class="hour">${formattedTime}</td>
+                    <td class="task description"><input type="text" id="task${hour}" /></td>
+                    <td class="saveBtn"><button onclick="saveTask(${hour})"><i class="fas fa-save"></i>
+                    </button></td> 
                  </tr>`;
+
   const $newRow = $(newRow); // Convert the string to a jQuery object
-  $newRow.find('.hour, .task input, .saveBtn').addClass(bgColorClass); // Add the specified class to elements
+  $newRow.find('.task input').addClass(bgColour); // Add background colour to task
   $(".container table").append($newRow);
 }
